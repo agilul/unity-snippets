559bbe053eeb75a20c0f3ef72b5b27f56ab426ab
Yat Hin Wong <agilulfo89@gmail.com>
2016-05-15 14:48:09 +0200

    Add files via upload

---
+++ Tint.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Tint : MonoBehaviour
+{
+    public Color tint;
+    [Range(0f, 1f)]
+    public float strength;
+
+    private SpriteRenderer[] renderers;
+
+	void Start()
+	{
+        renderers = GetComponentsInChildren<SpriteRenderer>();
+        SetShaderProperties();
+	}
+	
+	void Update()
+	{
+        SetShaderProperties();
+	}
+
+    private void SetShaderProperties()
+    {
+        foreach (SpriteRenderer r in renderers)
+        {
+            r.material.SetColor("_Color", tint);
+            r.material.SetFloat("_Strength", strength);
+        }
+    }
+}
